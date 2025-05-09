d5d242dea73f5ec0b788985d302599cc348a94e3
Cat drinking a cat <74091140+micyclebobert@users.noreply.github.com>
2025-05-09 22:11:22 +0600

    Added Seurat v4 and v5 preprocess

--- R/cTPnet_preprocess.R
+++ R/cTPnet_preprocess.R
@@ -28,6 +28,36 @@ preprocess_seurat3=function(seurat_data,dprotein){
 	return(data)
 }
 
+preprocess_seurat4=function(seurat_data,dprotein){
+    if (dprotein==12){
+    	data(gene_names_seurat3_protein12)
+    }else if (dprotein==24){
+    	data(gene_names_protein24)
+    }else{
+    	stop('Error: unrecognizable number of protein. Need to be 12 or 24\n')
+    }
+	data=as.data.frame(as.matrix(GetAssayData(seurat_data, slot = "counts")))
+	data=data[shared_gene,]
+	data[is.na(data)]=0.0
+	rownames(data)=shared_gene
+	return(data)
+}
+
+preprocess_seurat5=function(seurat_data,dprotein){
+    if (dprotein==12){
+    	data(gene_names_seurat3_protein12)
+    }else if (dprotein==24){
+    	data(gene_names_protein24)
+    }else{
+    	stop('Error: unrecognizable number of protein. Need to be 12 or 24\n')
+    }
+	data=as.data.frame(as.matrix(GetAssayData(seurat_data, slot = "counts")))
+	data=data[shared_gene,]
+	data[is.na(data)]=0.0
+	rownames(data)=shared_gene
+	return(data)
+}
+
 preprocess_matrix=function(matrix_data,dprotein){
     if (dprotein==12){
     	data(gene_names_protein12)
